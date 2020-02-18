0e7a699d4c52817cdf197022960c25f90814995d
Kenneth666 <kenneth666@Kenneth666deMacBook-Pro.local>
2020-02-18 15:45:21 +0800

    first commit

---
+++ my-work/week1/coding-foundation/script.js
@@ -0,0 +1,39 @@
+document.getElementById("n").addEventListener("click",main); // add function to submit button
+let n = 0;
+
+// function to create a box
+function drawBox(){
+  var box = document.createElement("div"); // create a <div> element
+  box.className="box"; //style the box
+  document.getElementById("mainBox").appendChild(box); //add box to the mainBox <div>
+}
+
+//function to remove box
+function removeBox(){
+  var elem = document.getElementById("box");
+  return elem.parentNode.removeChild(elem);
+}
+
+// the function to draw box according to the input number
+function main(){
+  var value = document.getElementById("myInput").value // extract number from input form
+  if(n===0){
+  for (var i = 0; i < value; i++){
+    drawBox();
+    n=value;
+  }
+}if(n>value){  // if the new input number is smaller than the current number of boxes-- remove the boxes
+    var increased = n-value;
+    for (var i = 0; i < increased; i++){
+      removeBox();
+    }
+    n = value;
+  }
+ if(n < value){ // if the new input number is larger than the current number of boxes-- add more boxes
+   var decreased = value - n;
+   for (var i = 0; i < decreased; i++){
+     drawBox();
+   }
+   n = value;
+ }
+}
